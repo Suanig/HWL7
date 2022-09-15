b4a738ad2563a1c194db75337def7e6d387be8c5
Suanig <suanig88@gmail.com>
2022-09-15 19:14:58 +0300

    Create HWL7

---
+++ HWL7T2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int raws = new Random().Next(1, 10);
+int columns = new Random().Next(1, 10);
+
+double[,] GetArray(int m, int n)
+{
+    var result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(-1000, 1000);
+        }
+    }
+    return result;
+}
+
+var array1 = GetArray(raws, columns);
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Console.Write("Введите первую координату элемента в массиве:  ");
+int coordX = Convert.ToInt32(Console.ReadLine());
+if (coordX > (raws - 1) || coordX < 0)
+{
+    Console.WriteLine("Элемента с заданной позицией в массиве нет");
+
+}
+else
+{
+    Console.Write("Введите вторую координату элемента в массиве:  ");
+    int coordY = Convert.ToInt32(Console.ReadLine());
+    if ((coordY > (columns - 1) || coordY < 0))
+    {
+        Console.WriteLine("Элемента с заданной позицией в массиве нет");
+    }
+    else 
+    {
+        PrintArray(array1);
+        Console.WriteLine();
+        Console.WriteLine($"Выбраннный элемент данного массива - {array1[coordX, coordY]}");
+    }
+}
+
+
---
+++ HWL7T3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int raws = new Random().Next(1, 10);
+int columns = new Random().Next(1, 10);
+
+double[,] GetArray(int m, int n)
+{
+    var result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(-1000, 1000);
+        }
+    }
+    return result;
+}
+
+
+double[] GetColumsSrA(double[,] arr)
+{
+    var result = new double[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum = sum + arr[j, i];
+        }
+        result[i] = Math.Round(sum / arr.GetLength(0), 2);
+
+    }
+    return result;
+}
+
+foreach (var item in GetColumsSrA(GetArray(raws, columns)))
+{
+    Console.WriteLine(item);
+}
+
+
